94e29e1e7fcd968e6bc99bbb87c27b5f3db5d769
rupesh <rupeshsaha899@gmail.com>
2026-02-14 16:28:13 +0530

    fix req array

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -115,8 +115,9 @@ class ApiController extends Controller
             'landmark' => 'nullable|string',
             'date' => 'required|date',
             'time' => 'required|string',
-            'requirements' => 'nullable|array|min:1',
-            'requirements.*' => 'string',
+            'requirements' => 'nullable|array',
+            'requirements.*' => 'array',
+            'requirements.*.*' => 'string',
         ]);
 
         if ($validator->fails()) {
